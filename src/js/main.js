9d31e71981f7aa683c47e144ce7406caa258d939
Camara-Mohamed <mohamed.camara1@student.hepl.be>
2024-11-26 15:28:26 +0100

    Exercice 26-11-24 v1

---
+++ src/js/main.js
@@ -0,0 +1,19 @@
+(function () {
+    const burgerMenu = {
+        btnElt: document.querySelector('.nav-button'),
+        mainElt: document.querySelector('main'),
+        init() {
+            document.documentElement.classList.add('js-enabled');
+            this.addEventListeners();
+        },
+        addEventListeners() {
+            this.btnElt.addEventListener('click', () => {
+                this.toggleMenu();
+            });
+        },
+        toggleMenu() {
+            this.mainElt.classList.toggle('is-opened');
+        }
+    };
+    burgerMenu.init();
+})();
